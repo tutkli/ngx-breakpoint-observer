4089c817fb07df9ad1b4965bcdeb73b5c37b4639
Clara Castillo <claracastillochale@gmail.com>
2024-04-30 01:19:11 +0200

    perf: cleanup code

--- libs/ngx-breakpoint-observer/src/lib/observe-media-query.ts
+++ libs/ngx-breakpoint-observer/src/lib/observe-media-query.ts
@@ -6,33 +6,29 @@ import { effect, type Signal, signal, untracked } from '@angular/core';
  * @param query
  */
 export function observeMediaQuery(query: string): Signal<boolean> {
-  const isSupported = signal(
-    window && 'matchMedia' in window && typeof window.matchMedia === 'function'
-  );
+  const isSupported =
+    window && 'matchMedia' in window && typeof window.matchMedia === 'function';
 
   let mediaQuery: MediaQueryList | undefined;
   const matches = signal(false);
 
   const cleanup = () => {
     if (!mediaQuery) return;
-
     mediaQuery.removeEventListener('change', update);
   };
 
   const update = () => {
-    if (!isSupported()) return;
-
+    if (!isSupported) return;
     mediaQuery = window.matchMedia(query);
     untracked(() => matches.set(!!mediaQuery?.matches));
 
     if (!mediaQuery) return;
-
     mediaQuery.addEventListener('change', update);
   };
 
   effect(onCleanup => {
     update();
-    onCleanup(() => cleanup());
+    onCleanup(cleanup);
   });
 
   return matches.asReadonly();
